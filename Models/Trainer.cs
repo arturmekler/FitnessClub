a553ff697c02e08fe09caf8d535611a3b5ed1169
arturmekler <artur.mekler@gmail.com>
2020-07-06 18:54:59 +0200

    add models

---
+++ Models/Trainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessClub.Models
+{
+    public class Trainer
+    {
+        public int TrainerID { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public char Sex { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessClub.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public char Sex { get; set; }
+    }
+}
