5f39951a2ed667da9aae837557371dbe2c32a925
arturmekler <artur.mekler@gmail.com>
2020-07-14 20:33:36 +0200

    add login and authorization components

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserScheduleComponent } from './user-schedule/user-schedule.component';
 import { ChatComponent } from './chat/chat.component';
 import { SignUpForClassesComponent } from './sign-up-for-classes/sign-up-for-classes.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import { SignUpForClassesComponent } from './sign-up-for-classes/sign-up-for-cla
     UserProfileComponent,
     UserScheduleComponent,
     ChatComponent,
-    SignUpForClassesComponent
+    SignUpForClassesComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -37,7 +41,9 @@ import { SignUpForClassesComponent } from './sign-up-for-classes/sign-up-for-cla
       { path: 'user-profile', component: UserProfileComponent },
       { path: 'user-schedule', component: UserScheduleComponent },
       { path: 'chat', component: ChatComponent },
-      { path: 'sign-up-for-classes', component: SignUpForClassesComponent }
+      { path: 'sign-up-for-classes', component: SignUpForClassesComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
     ])
   ],
   providers: [],
